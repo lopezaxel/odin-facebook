fcb4580e320741a4cb9316dc9ef1339dee2d58bb
Axel Lopez <lopezaxel@protonmail.com>
2020-09-03 11:27:17 -0300

    User must sign in to do anything

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
   devise_for :users, :controllers => {
     registrations: 'registrations'
   }
+
   resources :friend_requests, only: [:create, :destroy]
+  resources :posts
 end
