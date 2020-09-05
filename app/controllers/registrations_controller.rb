2af8c55ec39b631a8c472c56e7993c49b57b48b3
Axel Lopez <lopezaxel@protonmail.com>
2020-09-05 11:01:06 -0300

    Add birthday and gender to users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
   
     def sign_up_params
-      params.require(:user).permit(:name, :email, :password, :encrypted_password)
+      params.require(:user).permit(:name, :email, :password, :encrypted_password, :birthday, :gender)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_175411) do
+ActiveRecord::Schema.define(version: 2020_09_05_134716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_175411) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
-    #t.integer "friend_id"
+    t.integer "friend_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2020_09_03_175411) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.date "birthday"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
